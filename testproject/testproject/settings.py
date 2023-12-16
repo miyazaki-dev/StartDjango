d85260f7a9f7ad2ccea2d8ab8462c219065de169
miyazaki <sangi5911@icloud.com>
2023-12-16 12:38:35 +0900

    addJavascript
    css、javascript、画像の新規追加

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
--- testproject/webtestapp/views.py
+++ testproject/webtestapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     my_dict = {
-        'insert_something':"views.pyのinsert_something部分です。"
+        'insert_something':"views.pyのinsert_something部分です。",
+        'name':"miyazaki_dev"
     }
     return render(request,'webtestapp/index.html',my_dict)
